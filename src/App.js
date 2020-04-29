158d171a3e56f3c8373d749531476980d45b2b4d
Aaron Thompson <thompson_ad@outlook.com>
2020-04-29 14:37:31 +0100

    toggle tab display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from "react";
-import { Header } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Header, Container } from "semantic-ui-react";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <Header inverted as="h1">
-          Project Mini-Mek
-        </Header>
+import TabBarContainer from "features/tabs/TabBarContainer";
+import UnitInfo from "features/unitInfo/UnitInfo";
+import Pilots from "features/pilots/Pilots";
+import Mechs from "features/mechs/Mechs";
+import UnitOrganization from "features/unitOrganization/UnitOrganization";
+
+class App extends Component {
+  render() {
+    const tabs = [
+      { name: "unitInfo", label: "Unit Info", component: UnitInfo },
+      { name: "pilots", label: "Pilots", component: Pilots },
+      { name: "mechs", label: "Mechs", component: Mechs },
+      {
+        name: "unitOrganization",
+        label: "Unit Organization",
+        component: UnitOrganization,
+      },
+    ];
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Header inverted as="h1">
+            Project Mini-Mek
+          </Header>
+        </div>
+        <Container>
+          <TabBarContainer tabs={tabs} size="massive" />
+        </Container>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
