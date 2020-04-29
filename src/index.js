604011c1a0fc95083923db4af429a624e0236a4f
Aaron Thompson <thompson_ad@outlook.com>
2020-04-29 07:47:52 +0100

    testing a sample component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 
+import configureStore from "./store/configureStore";
+const store = configureStore();
+
+// The redux store is now accessible to any Redux-connected react component in the tree
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/sampleComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const mapState = (state) => ({
+  data: state.test.data,
+});
+
+class SampleComponent extends Component {
+  render() {
+    const { data } = this.props;
+
+    return <div className="">Data from Redux: {data}</div>;
+  }
+}
+
+export default connect(mapState)(SampleComponent);
